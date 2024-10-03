8ecd193f5cdd0b649c36cdb637a029a9d2ccf96b
marc-alary <121094501+marc-alary@users.noreply.github.com>
2024-10-03 13:19:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from ota import OTAUpdater
-from config import SSID, PASSWORD
-
-firmware_url = "https://raw.githubusercontent.com/marc-alary/TennisScorer/"
-
-ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
-ota_updater.download_and_install_update_if_available()
+###########################################################
+# Projet : TennisScorer
+# Carte : Pupitre
+# Date : 
+# Revision code : 
+###########################################################
