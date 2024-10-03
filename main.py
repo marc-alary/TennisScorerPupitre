3833aef095ee23812ca6520b66d27c2a555ef1d5
marc-alary <121094501+marc-alary@users.noreply.github.com>
2024-10-03 13:22:58 +0200

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-#from ota import OTAUpdater
-#from config import SSID, PASSWORD
-
-#firmware_url = "https://raw.githubusercontent.com/marc-alary/TennisScorer/"
-
-#ota_updater = OTAUpdater(SSID, PASSWORD, firmware_url, "test.py")
-#ota_updater.download_and_install_update_if_available()
