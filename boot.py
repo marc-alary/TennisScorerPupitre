4b5e1b0a05e1f1558bef5e7b7db17a8648285082
marc-alary <121094501+marc-alary@users.noreply.github.com>
2025-06-26 11:12:06 +0200

    Update boot.py

--- boot.py
+++ boot.py
@@ -11,7 +11,8 @@ wlan.active(True)
 BP_RESET_SCORE = Pin(19, Pin.IN, Pin.PULL_DOWN)
 
 if BP_RESET_SCORE.value()==1:
-    wlan.connect("Marcus","cf08cfa03572")
+    # wlan.connect("Marcus","cf08cfa03572")
+    wlan.connect("BTHOTSPOT-4919","TCS82410")
     essais = 0
     time.sleep(2)
     clear_screen()
